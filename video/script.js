cfe7d2933804d36a4baa7e4f4c933c4dc34e5719
monawang09 <wangtangbaihe@163.com>
2023-04-20 23:41:49 -0700

    Add video project

---
+++ video/script.js
@@ -0,0 +1,11 @@
+const element = document.getElementById("myVideo");
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const halfwidth = 0.5*window.innerWidth;
+  const halfheight = 0.5*window.innerHeight;
+  const blurAmount = ((x - halfwidth)*(x - halfwidth) + (y - halfheight)*(y - halfheight))/ (halfwidth * halfwidth) * 10; 
+  element.style.filter = `blur(${blurAmount}px)`;
+});
